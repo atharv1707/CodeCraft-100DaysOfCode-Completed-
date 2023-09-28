61d9369a803a47fac21c937fe7d578bd212b6cca
Atharv Pal <77221646+atharv1707@users.noreply.github.com>
2023-09-28 21:09:35 +0530

    Add files via upload

---
+++ HammingDistance.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+
+        int res = x^y;
+        int count = 0 ; 
+
+         while(res > 0){
+             //checking least sig bit of the res
+             if(res & 1){
+                 count ++;
+             }
+
+             res >>= 1;
+
+         }
+         return count;
+
+        
+    }
+};
