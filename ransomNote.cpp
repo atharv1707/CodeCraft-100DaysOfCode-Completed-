2f80546e2a8dfe2a14c808b9a36cc63829675ef2
Atharv Pal <77221646+atharv1707@users.noreply.github.com>
2023-10-03 22:52:56 +0530

    Update ransomNote.cpp

--- ransomNote.cpp
+++ ransomNote.cpp
@@ -20,3 +20,25 @@ public:
         return true;  // All characters in ransomNote can be constructed from the magazine
     }
 };
+
+
+
+//second solution
+
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+
+        vector<int> count(26,0);
+        for(char ch : magazine)
+            count[ch - 'a']++;
+        
+        for(char ch : ransomNote)
+            if(count[ch - 'a']-- <= 0)
+                return false;
+        
+        return true;
+        
+    }
+};
