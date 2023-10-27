4826b0a34e8bb49af5f81a96ba16cd61199d64e8
Atharv Pal <77221646+atharv1707@users.noreply.github.com>
2023-10-28 00:19:50 +0530

    Update Sum of Square Numbers.cpp

--- Sum of Square Numbers.cpp
+++ Sum of Square Numbers.cpp
@@ -1,3 +1,5 @@
+//we notice that both are interdependent via c
+
 class Solution {
 public:
     bool judgeSquareSum(int c) {
@@ -18,3 +20,36 @@ public:
         
     }
 };
+
+//two-pointer search.
+//I guess I should start using long long in general hehe
+
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+
+        long s = 0 ; 
+        long e = sqrt(c);
+        long long sum = 0 ; 
+
+        while(s <= e){
+            
+            sum = s*s + e*e;
+            if(sum ==c){
+                return true;
+            }
+
+            else if(sum > c){
+                e--;
+            }
+            
+            else{
+                s++;
+            }
+        }
+
+        return false;
+        
+    }
+};
+
