a1ae32bc5effb04ad3b0b3c972701fb200bf8992
Atharv Pal <77221646+atharv1707@users.noreply.github.com>
2023-10-28 22:55:19 +0530

    Update nth magical number.cpp

--- nth magical number.cpp
+++ nth magical number.cpp
@@ -1,29 +1,6 @@
 #define ll long long
 
 class Solution {
-public:
-    int nthMagicalNumber(int n, int a, int b) {
-        ll mod = 1e9+7;
-        ll low = 1, mid;
-        ll high = static_cast<ll>(min(a, b)) * n; // Use 'll' for 'high' and 'static_cast'
-
-         // Initialize low and high
-        ll lcm_ab = lcm(a, b);
-
-        while (low < high) { // Use '<' instead of '<='
-            mid = low + (high - low) / 2;
-            ll target = mid / a + mid / b - mid / lcm_ab;
-
-            if (target < n) {
-                low = mid + 1;
-            } else {
-                high = mid;
-            }
-        }
-
-        return (high % mod);
-    }
-    
 private:
     ll gcd(ll a, ll b) {
         if (b == 0) return a;
@@ -31,6 +8,29 @@ private:
     }
 
     ll lcm(ll a, ll b) {
-        return a / gcd(a, b) * b;
+        return (a * b) / gcd(a, b);
+    }
+
+public:
+    int nthMagicalNumber(int n, int a, int b) {
+        ll mod = 1e9 + 7;
+        ll low = 1;
+        ll mid;
+        ll high = static_cast<ll>(min(a, b) ) * n; //narrowing the search space 
+        ll target;
+        ll lcm_ab = lcm(a, b);
+
+        while (low < high) {
+            mid = low + (high - low) / 2;
+            target = mid / a + mid / b - mid / lcm_ab; // 
+
+            if (target >= n) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return static_cast<int>(high % mod);
     }
 };
