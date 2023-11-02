61d0c6edf85cb3017b2d08063dac9642ffdde9a3
Atharv Pal <77221646+atharv1707@users.noreply.github.com>
2023-11-02 23:09:01 +0530

    Linked List

---
+++ Remove_elements_LL.cpp
@@ -0,0 +1,45 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* dummy = new ListNode;
+        dummy->next = head;
+
+        ListNode* current = dummy;
+        while(current->next){
+            if(current->next->val == val){
+                current->next = current->next->next;
+            }
+            else{
+                current = current->next;
+            }
+        }
+
+        return dummy->next;
+        
+    }
+};
+{
+    printf("Hello World");
+
+    return 0;
+}
