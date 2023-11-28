f5a2f8a96c8b6f8a9ed11ac307df39af061853c2
Atharv Pal <77221646+atharv1707@users.noreply.github.com>
2023-11-28 21:43:56 +0530

    Update ContaintsDuplicate2.cpp

--- ContaintsDuplicate2.cpp
+++ ContaintsDuplicate2.cpp
@@ -18,3 +18,35 @@ public:
         
     
 };
+
+
+//approach 2
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_set<int> store;
+
+        int i = 0;
+        int j = 0;
+
+        for(j = 0 ; j < nums.size();j++){
+            if(j-i > k){
+                store.erase(nums[i]);
+                i++;
+            }
+
+            //that means we are in the correct window size
+            //now we check for the duplicate
+            if(store.find(nums[j]) != store.end()){
+                //if we are in the window size , and we found a duplicate , that means we return true;
+                return true;
+            }
+
+            store.insert(nums[j]);
+        }
+
+        return false;
+        
+    }
+};
