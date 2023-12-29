cf09ff2cc0a11358e273d72c957f10526994c0cb
Atharv Pal <77221646+atharv1707@users.noreply.github.com>
2023-12-30 01:19:53 +0530

    Update Invert Binary Tree.cpp

--- Invert Binary Tree.cpp
+++ Invert Binary Tree.cpp
@@ -27,3 +27,52 @@ public:
         
     }
 };
+
+
+//BFS 
+
+
+#include <queue>
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (!root) {
+            return NULL;
+        }
+
+        std::queue<TreeNode*> queue;
+        queue.push(root);
+
+        while (!queue.empty()) {
+            TreeNode* node = queue.front();
+            queue.pop();
+
+            TreeNode* temp = node->left;
+            node->left = node->right;
+            node->right = temp;
+
+            if (node->left != NULL) {
+                queue.push(node->left);
+            }
+
+            if (node->right != NULL) {
+                queue.push(node->right);
+            }
+        }
+
+        return root;
+    }
+};
