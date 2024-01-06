fc87d637ffc7c2c2fb104ce8c08fc10dcd386c8b
Atharv Pal <77221646+atharv1707@users.noreply.github.com>
2024-01-06 23:48:29 +0530

    Binary Tree

---
+++ Root Equals Sum of Children.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool checkTree(TreeNode* root) {
+
+        if(!root){
+            return true;
+        }
+
+        // int Root = root->val;
+
+        // bool left  =  checkTree(root->left);
+        // bool right =  checkTree(root->right);
+
+        return (root->val == (root->left)->val + (root->right)->val);
+
+
+        
+    }
+};
