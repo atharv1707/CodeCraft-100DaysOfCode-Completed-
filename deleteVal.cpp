7d72bacab9bf6954bf0def96ce1ae7cadb7da25c
Atharv Pal <77221646+atharv1707@users.noreply.github.com>
2024-05-05 22:06:27 +0530

    Create deleteVal.cpp

---
+++ deleteVal.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+
+        ListNode* prev = NULL;
+
+        while(node && node->next){
+
+            node->val = node->next->val;
+            prev = node;
+            node = node->next;
+
+        }
+
+        prev->next = NULL;
+
+        
+        
+    }
+};
